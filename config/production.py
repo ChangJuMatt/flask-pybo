aace22e8ee2fe2a9e2e4f9604023ebe2addf2d00
오창주 <revsheva1989@gmail.com>
2022-05-20 18:15:24 +0900

    production.py 수정

--- config/production.py
+++ config/production.py
@@ -1,6 +1,7 @@
 from logging.config import dictConfig
 from config.default import *
 
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
@@ -28,3 +29,13 @@ dictConfig({
         'handlers': ['file']
     }
 })
+
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
